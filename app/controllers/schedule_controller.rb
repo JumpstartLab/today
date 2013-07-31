74d643ff7063257025a351daba39e024c2081131
Franklin Webber <franklin.webber@gmail.com>
2013-07-31 15:49:27 -0600

    No date param will be considered today and MissingOutline will now be created with the date in mind

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -5,11 +5,17 @@ class ScheduleController < ApplicationController
   end
 
   def outline
-    Outline.find_by_publish_date(date_param) || Outline.today || MissingOutline.new
+    Outline.find_by_publish_date(date_param) || MissingOutline.new(date_param)
   end
 
   def date_param
-    DateTime.parse(params[:date_string]).to_date
+
+    if params[:date_string]
+      DateTime.parse(params[:date_string]).to_date
+    else
+      DateTime.now.to_date
+    end
+
   rescue
     nil
   end
