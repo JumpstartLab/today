cbccdfa5342a2ce94b7dd63df5bf308fc92a21db
Franklin Webber <franklin.webber@gmail.com>
2013-07-31 15:38:58 -0600

    Current date becomes editable when selected and moves to date on focus out

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,18 +1,24 @@
 module ScheduleHelper
 
+  def nav_date_format(date)
+    date.strftime("%m/%d/%y")
+  end
+
   def previous_link(outline)
     unless outline.is_a?(MissingOutline)
-      link_to outline.publish_date, schedule_path(outline), class: "ss-navigateleft left"
+      # link_to nav_date_format(outline.publish_date), schedule_path(outline), class: "ss-navigateleft left schedule-previous"
+      link_to "Back", schedule_path(outline), class: "ss-navigateleft left schedule-previous"
     else
-      link_to "The Beginning", "#"
+      link_to "The Beginning", "#", class: "schedule-beginning"
     end
   end
 
   def next_link(outline)
     unless outline.is_a?(MissingOutline)
-      link_to outline.publish_date, schedule_path(outline), class: "ss-navigateright right"
+      # link_to nav_date_format(outline.publish_date), schedule_path(outline), class: "ss-navigateright right schedule-next"
+      link_to "Forward", schedule_path(outline), class: "ss-navigateright right schedule-next"
     else
-      link_to "The End", "#"
+      link_to "The End", "#", class: "schedule-end"
     end
   end
 
